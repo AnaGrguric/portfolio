eb3500a5159e66b38a8ac66b107b84efb9332001
AnaGrguric <61662136+AnaGrguric@users.noreply.github.com>
2021-08-25 10:13:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
+let mouseCursor = document.querySelector(".cursor");
+let elements = document.querySelectorAll(".el");
+let navEl = document.querySelectorAll(".el-nav");
+
+window.addEventListener("mousemove", cursor)
+  
+function cursor(e){
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
+
+
+elements.forEach(element => {
+  element.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("cursor-scale");
+    
+  })
+  element.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("cursor-scale");
+    
+  })
+})
+
+navEl.forEach(element => {
+  element.addEventListener("mouseover", ()=> {
+    mouseCursor.classList.add("cursor-nav");
+  });
+  element.addEventListener("mouseleave", ()=> {
+    mouseCursor.classList.remove("cursor-nav");
+  });
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
   }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  }
+  } 
 
 
